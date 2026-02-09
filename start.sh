b0f1d6a9b4fa8becc546b536a387676aea01714d
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2026-02-08 18:40:47 -0800

    bonks and remove screen

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 git pull origin master
 git submodule update --init --recursive
 dotnet build DiscordAutoThreadBot.sln --configuration Release -o ./bin/live_release
-screen -dmS autothreadbot dotnet bin/live_release/DiscordAutoThreadBot.dll $1
+dotnet bin/live_release/DiscordAutoThreadBot.dll $1
